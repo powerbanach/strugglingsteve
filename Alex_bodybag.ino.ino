7e21781af536cf11f0d455133b1f8f627dbc6c53
powerbanach <115719482+powerbanach@users.noreply.github.com>
2022-10-14 01:28:25 +0200

    Update Alex_bodybag.ino.ino
    Cleaned up the mess a little

--- Alex_bodybag.ino.ino
+++ Alex_bodybag.ino.ino
@@ -19,8 +19,8 @@ int duration = 1000; // duration of the interval in miliseconds
 // servos minimum and maximum position
 #define MIN_POS 800 // the minuimum pulse width for your servos
 #define MAX_POS 2200 // maximum pulse width for your servos
-#define MIN_TIM 3000 // minimum time to wait between loops
-#define MAX_TIM 7000 // maximum time to wait between loops
+#define MIN_TIM 8000 // minimum time to wait between loops
+#define MAX_TIM 12000 // maximum time to wait between loops
  
 Servo backServo;
 Servo frontServo;
@@ -36,17 +36,36 @@ int d1;
 int d0_sh; // destination0 to be smoothed
 int d1_sh;
 int delayBetweenStruggles;
-int loop_num;
-bool reachedDestination = true;
 String msg = "";
 int movementsBetweenStruggles = 10;
 
-bool testExtremes = true;
+bool testExtremes = false;
 
 // the filter to be aplied
 // this value will be multiplied by "d0" and added to "d0_sh"
 float filtro = 0.1; // 0.01 to 1.0
- 
+
+// Moves the frontServo and the backServo to the target position
+void moveToPositionSmoothly(int posTo0, int posTo1) {  // , int posFrom0, int posFrom1 
+  int newPos0 = frontServo.read(); //posFrom0;
+  int newPos1 = backServo.read(); //posFrom1;
+  float stepInterval = 0.1;
+  int numberOfSteps = 15;
+  // 15 steps of 0.1 gets you 80% to the target position
+
+  for (int n=0; n<15; n++) {
+    newPos0 = newPos0 * (1.0-stepInterval) + posTo0 * stepInterval;
+    newPos1 = newPos1 * (1.0-stepInterval) + posTo1 * stepInterval;
+
+    // assign new position to the servo
+    frontServo.write(newPos0);
+    backServo.write(newPos1);
+
+    // delay to make the servo move
+    delay(20);
+  }
+}
+
 // setup runs once, when the sketch starts
 void setup() {
   // set sound pin
@@ -55,76 +74,42 @@ void setup() {
   Serial.begin(9600);
   frontServo.attach(9);
   backServo.attach(10);
-
-  if (testExtremes == false) {
-    d0_sh = random(MIN_POS, MAX_POS);
-    d1_sh = random(MIN_POS, MAX_POS);
-    d0 = random(MIN_POS, MAX_POS);
-    d1 = random(MIN_POS, MAX_POS);
-    frontServo.write(d0_sh);
-    backServo.write(d1_sh);
-    reachedDestination = false;
-    Serial.println("Init done! :-)");
-  }
+  
+  // Initialize the starting position to be centered (previously random(MIN_POS, MAX_POS))
+  d0_sh = (1.0*MIN_POS + 1.0*MAX_POS)/2.0;
+  d1_sh = (1.0*MIN_POS + 1.0*MAX_POS)/2.0;
+  frontServo.write(d0_sh);
+  backServo.write(d1_sh);
+  Serial.println("Init done! Show begins in 5, 4, 3, 2, 1");
+  delay(5000);
 }
  
 // main program loop, executes forever after setup()
 void loop() {
-  delay(5000);
+  
 
   if (testExtremes) {
     d0 = MIN_POS;
     d1 = MIN_POS;
     frontServo.write(MIN_POS);
     backServo.write(MIN_POS);
-    delay(3000);
+    delay(2000);
     frontServo.write(MAX_POS);
     backServo.write(MAX_POS);
-
-    delay(3000);
-    d0_sh = MAX_POS;
-    d1_sh = MAX_POS;
+    delay(5000);
     // the same thing but with smoothening, maybe that's the issue
-    for (int n=0; n<15; n++) {
-      d0_sh = d0_sh * (1.0-filtro) + d0 * filtro;
-      d1_sh = d1_sh * (1.0-filtro) + d1 * filtro;
-      // assign new position to the servo
-      frontServo.write(d0_sh);
-      backServo.write(d1_sh);
-      // delay to make the servo move
-      delay(20);
-    }
-
+    moveToPositionSmoothly(MIN_POS,MIN_POS);
+    delay(2000);
+    moveToPositionSmoothly(MAX_POS,MAX_POS);
+    delay(5000);
   }
   else {
-    //int sensorValue = analogRead(A2);
     for (int i=0; i<movementsBetweenStruggles; i++) {
-        
-        //digitalWrite(soundPin, LOW);
-        //delay(1000);
-        //digitalWrite(soundPin, HIGH);
-
-        if (reachedDestination) {
-          // calculate a new random position between max and min values
-          d0 = random(MIN_POS, MAX_POS);
-          d1 = random(MIN_POS, MAX_POS);
-          reachedDestination = false;
-        }
-    
-        // smooth the destination value
-        for (int n=0; n<15; n++) {
-          d0_sh = d0_sh * (1.0-filtro) + d0 * filtro;
-          d1_sh = d1_sh * (1.0-filtro) + d1 * filtro;
-      
-          // assign new position to the servo
-          frontServo.write(d0_sh);
-          backServo.write(d1_sh);
-
-          // delay to make the servo move
-          delay(20);
-        }
-        reachedDestination = true;
-        
+      // calculate a new random position between max and min values
+      d0 = random(MIN_POS, MAX_POS);
+      d1 = random(MIN_POS, MAX_POS);
+      moveToPositionSmoothly(d0,d1);
+      delay(300);
     }     
     delayBetweenStruggles = random(MIN_TIM, MAX_TIM);
     Serial.println("A new struggle begins");
